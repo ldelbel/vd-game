4633881a99c01e1e26f6ef4f256f13e80c9ac9b6
ldelbel <lucdelbel@gmail.com>
2020-12-11 10:20:20 -0300

    Add virus, test collison between antibody and virus

--- src/scenes/gamescene.js
+++ src/scenes/gamescene.js
@@ -9,19 +9,27 @@ export class GameScene extends Phaser.Scene {
     this.load.image('red-cell', '../assets/red-cell.png');
     this.load.image('white-cell', '../assets/white-cell.png');
     this.load.image('antibody1', '../assets/antibody1.png');
+    this.load.image('virus1', '../assets/virus-1.png');
+    this.load.image('virus2', '../assets/virus-2.png');
   }
   
   create() {
-
+    // Install Weapon Plugin
     this.plugins.installScenePlugin(
       'WeaponPlugin',
       WeaponPlugin.WeaponPlugin,
       'weapons',
       this
   );
+
+    // Defining important variables
+    gameState.gameWidth = this.game.config.width;
+    gameState.gameHeight = this.game.config.height;
+
     // background
     const background = this.add.image(0, -40, 'background').setScale(0.367);
     background.setOrigin(0,0);
+    background.depth = -2;
 
     // create elements
     const redCells = this.physics.add.group();
@@ -34,15 +42,17 @@ export class GameScene extends Phaser.Scene {
       const yCoord = Math.random() * 320 + 160;
       const cell = whiteCells.create(1200, yCoord,'white-cell');
       cell.setScale(0.015);
-      cell.alpha = 0.6;
+      cell.alpha = 0.5;
       cell.setVelocityX(-200);
+      cell.depth = -1;
     };
     function redCellCreate() {
       const yCoord = Math.random() * 320 + 160;
       const cell = redCells.create(1200, yCoord,'red-cell');
       cell.setScale(0.04);
-      cell.alpha = 0.6;
+      cell.alpha = 0.5;
       cell.setVelocityX(-200);
+      cell.depth = -1;
     };
     const redCellLoop = this.time.addEvent({
       delay: 250,
@@ -57,24 +67,75 @@ export class GameScene extends Phaser.Scene {
       loop: true
     });    
 
-    // firing animation
-    gameState.antibody = this.add.weapon(30, 'antibody1')
-    // gameState.antibody.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-    gameState.antibody.bulletAngleOffset = 180;
-    gameState.antibody.bulletSpeed = 400;
-    gameState.antibody.fireRate = 400;
-    gameState.antibody.trackSprite(gameState.lympho,0,0)
-    // gameState.fireAntibody = function antibodyCreate() {
-    //   const xCoord = gameState.lympho.x;
-    //   const yCoord = gameState.lympho.y;
-    //   const antibody = gameState.antibodies.create(xCoord, yCoord,'antibody1');
-    //   antibody.setScale(0.1);
-    //   antibody.rotation = Math.PI/2;
-    //   antibody.alpha = 0.9;
-    //   antibody.setVelocityX(500);
-    //   antibody.body.setAllowGravity(false)
-    // };
+    // firing antibodies    
+    gameState.antibody1 = this.add.weapon(30, 'antibody1');
+    gameState.antibody1.bulletAngleOffset = 90;
+    gameState.antibody1.bulletSpeed = 400;
+    gameState.antibody1.fireAngle = 0;
+    gameState.antibody1.fireRate = 400;
+    gameState.antibody1.trackSprite(gameState.lympho,0,0);
+    gameState.antibody1.bullets.children.entries.forEach( bullet => {
+      bullet.scale = 0.1
+      bullet.body.allowGravity = false
+    })
 
+    gameState.antibody2 = this.add.weapon(30, 'antibody2');
+    gameState.antibody2.bulletAngleOffset = 90;
+    gameState.antibody2.bulletSpeed = 400;
+    gameState.antibody2.fireAngle = 0;
+    gameState.antibody2.fireRate = 400;
+    gameState.antibody2.trackSprite(gameState.lympho,0,0);
+    gameState.antibody2.bullets.children.entries.forEach( bullet => {
+      bullet.scale = 0.1
+      bullet.body.allowGravity = false
+    })
+
+    gameState.currentAntibody = gameState.antibody1;
+
+    function switchAntibody() {
+      if(gameState.currentAntibody == gameState.antibody1) {
+        gameState.currentAntibody = gameState.antibody2;
+      } else if(gameState.currentAntibody == gameState.antibody2){
+        gameState.currentAntibody = gameState.antibody1;
+      }
+    }
+
+    // Enemies
+    gameState.virus1 = this.physics.add.group();
+    gameState.virus2 = this.physics.add.group();
+
+
+    let enemy1 =  gameState.virus1.create(gameState.gameWidth - 100, gameState.gameHeight/2 - 30, 'virus1').setScale(0.3)
+    enemy1.life = 30;
+    enemy1.body.allowGravity = false;
+    enemy1.setImmovable(true);
+    let enemy2 =  gameState.virus2.create(gameState.gameWidth - 100, gameState.gameHeight/2 + 40, 'virus2').setScale(0.3)
+    enemy2.life = 30;
+    enemy2.body.allowGravity = false;
+    enemy2.setImmovable(true);
+
+
+    this.physics.add.collider(gameState.virus1,  gameState.antibody1.bullets, hitVirusHard, null, this);
+    this.physics.add.collider(gameState.virus2,  gameState.antibody1.bullets, hitVirusSoft, null, this);
+    this.physics.add.collider(gameState.virus1,  gameState.antibody2.bullets, hitVirusSoft, null, this);
+    this.physics.add.collider(gameState.virus2,  gameState.antibody2.bullets, hitVirusHard, null, this);
+     
+    
+    function hitVirusHard(antibody, virus) {
+      antibody.destroy();
+      virus.life -= 10;
+      if(virus.life <= 0){
+        virus.destroy()
+      }
+    }
+
+    function hitVirusSoft(antibody, virus) {
+      antibody.destroy();
+      virus.life -= 5;
+      if(virus.life <= 0){
+        virus.destroy()
+      }
+    }
 
 
   }
@@ -82,14 +143,23 @@ export class GameScene extends Phaser.Scene {
   update() {
       if(gameState.cursors.right.isDown) {
         gameState.lympho.x += 10;
-      } else if(gameState.cursors.up.isDown) {
+      } 
+      if(gameState.cursors.up.isDown) {
         gameState.lympho.y -= 15;
-      } else if(gameState.cursors.left.isDown) {
+      } 
+      if(gameState.cursors.left.isDown) {
         gameState.lympho.x -= 10;
-      } else if(gameState.cursors.down.isDown) {
+      } 
+      if(gameState.cursors.down.isDown) {
         gameState.lympho.y += 15;
-      } else if(gameState.cursors.space.isDown) {
-        gameState.antibody.fire()
+      } 
+       if(gameState.cursors.space.isDown) {
+        if(gameState.currentAntibody == gameState.antibody1){
+          gameState.antibody1.fire();
+        } else if(gameState.currentAntibody == gameState.antibody2) {
+          gameState.antibody2.fire();
+        }
+
       }
     }
 }
