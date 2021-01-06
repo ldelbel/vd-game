1b774d5ceba080ca8df261477a116970d50f6ec5
ldelbel <lucdelbel@gmail.com>
2021-01-06 07:45:04 -0300

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,15 @@
 const path = require('path');
-<<<<<<< HEAD
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
 
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
   },
 
   module: {
@@ -22,71 +21,39 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-          }
-        }
-      }
-    ]
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
-    open: true
+    open: true,
   },
 
   plugins: [
     new CopyWebpackPlugin(
-      { 
+      {
         patterns: [
           {
             from: path.resolve(__dirname, 'index.html'),
-            to: path.resolve(__dirname, 'build')
+            to: path.resolve(__dirname, 'build'),
           },
           {
             from: path.resolve(__dirname, 'assets', '**', '*'),
-            to: path.resolve(__dirname, 'build')
-          }
-        ]
-      }
+            to: path.resolve(__dirname, 'build'),
+          },
+        ],
+      },
     ),
     new webpack.DefinePlugin({
       'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true)
-    })
-  ]
-}
-=======
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'developemnt',
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(jpe?g||png||svg||gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[hash:6].[ext]',
-          outputPath: 'images',
-          publicPath: 'images',
-          emitFile: true,
-          esModule: false,
-        },
-      },
-
-    ],
-  },
-
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+  ],
 };
->>>>>>> 1290ed9472e85b6d67b7ce5b79674476b0538905
